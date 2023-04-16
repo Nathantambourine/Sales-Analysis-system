7d1b9210dd229df23d729a3c7e67e3b83ab55fde
Nathantambourine <130502116+Nathantambourine@users.noreply.github.com>
2023-04-16 17:24:07 -0400

    Add files via upload

---
+++ Benford's Law Assignment/Benford's Law.py
@@ -0,0 +1,10 @@
+def printMenu():
+    print('''
+          Customer and Sales System\n
+          1. Enter Customer Information\n
+          2. Generate Customer data file\n
+          3. Report on total Sales (Not done in this part)\n
+          4. Check for fraud in sales data (Not done in this part)\n
+          9. Quit\n
+          Enter menu option (1-9)
+          ''')
